d76a107ae76395a4f4182263dcf6b778c75f3595
takumon <inouetakumon@gmail.com>
2017-07-05 07:54:10 +0900

    チュートリアル 5.Service終了

--- angular-quickstart/src/app/app.component.ts
+++ angular-quickstart/src/app/app.component.ts
@@ -1,19 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './Hero';
+import { HeroService } from './hero.service';
 
-
-const HEROES: Hero[] = [
-  {id: 11, name: '井上　太郎'},
-  {id: 12, name: '佐々木　敬太'},
-  {id: 13, name: '清水　翔太'},
-  {id: 14, name: '佐倉　三郎'},
-  {id: 15, name: '藤原　俊介'},
-  {id: 16, name: '財前　良平'},
-  {id: 17, name: '岸　太郎'},
-  {id: 18, name: '斎藤　政宗'},
-  {id: 19, name: '織田　雄平'},
-  {id: 20, name: '萩原　明宏'},
-];
 
 @Component({
   selector: 'my-app',
@@ -78,13 +66,24 @@ const HEROES: Hero[] = [
       margin-right: .8em;
       border-radius: 4px 0 0 4px;
     }
-  `]
+  `],
+  providers: [HeroService]
 })
 
-export class AppComponent  {
+export class AppComponent  implements OnInit{
   title = 'Tour of Heroes';
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
---
+++ angular-quickstart/src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+
+export const HEROES: Hero[] = [
+  {id: 11, name: '井上　太郎'},
+  {id: 12, name: '佐々木　敬太'},
+  {id: 13, name: '清水　翔太'},
+  {id: 14, name: '佐倉　三郎'},
+  {id: 15, name: '藤原　俊介'},
+  {id: 16, name: '財前　良平'},
+  {id: 17, name: '岸　太郎'},
+  {id: 18, name: '斎藤　政宗'},
+  {id: 19, name: '織田　雄平'},
+  {id: 20, name: '萩原　明宏'},
+];
