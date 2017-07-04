471650b88ccc283da16ed0f6b380c775531a04a4
takumon <inouetakumon@gmail.com>
2017-07-04 23:31:33 +0900

    チュートリアル 3.Master/Detail終了

--- angular-quickstart/src/app/app.component.ts
+++ angular-quickstart/src/app/app.component.ts
@@ -1,7 +1,102 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
+const HEROES: Hero[] = [
+  {id: 11, name: '井上　太郎'},
+  {id: 12, name: '佐々木　敬太'},
+  {id: 13, name: '清水　翔太'},
+  {id: 14, name: '佐倉　三郎'},
+  {id: 15, name: '藤原　俊介'},
+  {id: 16, name: '財前　良平'},
+  {id: 17, name: '岸　太郎'},
+  {id: 18, name: '斎藤　政宗'},
+  {id: 19, name: '織田　雄平'},
+  {id: 20, name: '萩原　明宏'},
+];
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes"
+          [class.selected]="hero === selectedHero"
+          (click)="onSelect(hero)"
+          >
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+    <div *ngIf="selectedHero">
+      <h2>{{selectedHero.name}} details!</h2>
+      <div><label>id :</label>{{selectedHero.id}}</div>
+      <div>
+        <label>name :</label>
+        <input [(ngModel)]="selectedHero.name" placeholder="name" />
+      </div>
+    </div>
+  `,
+
+  styles: [`
+    .selected {
+      background-color: #CFD8DC !important;
+      color: white;
+    }
+    .heroes {
+      margin: 0 0 2em 0;
+      list-style-type: none;
+      padding: 0;
+      width: 15em;
+    }
+    .heroes li {
+      cursor: pointer;
+      position: relative;
+      left: 0;
+      background-color: #EEE;
+      margin: .5em;
+      padding: .3em 0;
+      height: 1.6em;
+      border-radius: 4px;
+    }
+    .heroes li.selected:hover {
+      background-color: #BBD8DC !important;
+      color: white;
+    }
+    .heroes li:hover {
+      color: #607D8B;
+      background-color: #DDD;
+      left: .1em;
+    }
+    .heroes .text {
+      position: relative;
+      top: -3px;
+    }
+    .heroes .badge {
+      display: inline-block;
+      font-size: small;
+      color: white;
+      padding: 0.8em 0.7em 0 0.7em;
+      background-color: #607D8B;
+      line-height: 1em;
+      position: relative;
+      left: -1px;
+      top: -4px;
+      height: 1.8em;
+      margin-right: .8em;
+      border-radius: 4px 0 0 4px;
+    }
+  `]
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent  {
+  title = 'Tour of Heroes';
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
