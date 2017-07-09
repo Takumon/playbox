d873ac4f635575a856b4cdc8caa43b19c3b96685
takumon <inouetakumon@gmail.com>
2017-07-09 22:27:39 +0900

    TODOアプリ作成

--- ng4-todo-app/src/app/app.component.ts
+++ ng4-todo-app/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { todoList }  from './todo-list';
+import { Todo }      from './todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Todo';
+  todoList = todoList
+
+  addTodo(todo: Todo) {
+    this.todoList.unshift(todo);
+  }
 }
--- ng4-todo-app/src/app/app.module.ts
+++ ng4-todo-app/src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MaterialModule } from '@angular/material';
+
 
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoFormComponent } from './todo-form/todo-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule,
+
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
