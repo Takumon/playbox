3e6dcf2116a460308d27ce459ba537e79517cb8d
takumon <inouetakumon@gmail.com>
2017-07-15 15:00:26 +0900

    linter指摘修正

--- ng4-mean/server/bin/www.ts
+++ ng4-mean/server/bin/www.ts
@@ -1,13 +1,13 @@
-import * as http from "http";
-import { app } from "../app";
-import { serverPort } from "../config";
+import * as http from 'http';
+import { app } from '../app';
+import { serverPort } from '../config';
 
 
 /**
  * ポートの設定.
  */
 const port = normalizePort(process.env.PORT || serverPort);
-app.set("port", port);
+app.set('port', port);
 
 
 /**
@@ -20,8 +20,8 @@ const server = http.createServer(app);
  * HTTPサーバポート設定.
  */
 server.listen(port, () => console.log(`API running on localhost:${port}`));
-server.on("error", onError);
-server.on("listening", onListening);
+server.on('error', onError);
+server.on('listening', onListening);
 
 /**
  * ポートを正規化
@@ -48,22 +48,22 @@ function normalizePort(val): boolean | number {
  */
 
 function onError(error) {
-  if (error.syscall !== "listen") {
+  if (error.syscall !== 'listen') {
     throw error;
   }
 
-  const bind = typeof port === "string"
-    ? "Pipe " + port
-    : "Port " + port;
+  const bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges");
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
       process.exit(1);
       break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use");
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
       process.exit(1);
       break;
     default:
@@ -73,7 +73,7 @@ function onError(error) {
 
 function onListening() {
   const addr = server.address();
-  const bind = typeof addr === "string"
-    ? "pipe " + addr
-    : "port " + addr.port;
+  const bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
 }
--- ng4-mean/server/routes/api.ts
+++ ng4-mean/server/routes/api.ts
@@ -2,13 +2,13 @@
 import * as path from 'path';
 import * as http from 'http';
 import * as bodyParser from 'body-parser';
-import { Request, Response, Router } from "express";
+import { Request, Response, Router } from 'express';
 import axios from 'axios';
 
 const apiRouter: Router = Router();
 
 // declare axios for making http requests
-const API: string = 'https://jsonplaceholder.typicode.com';
+const API = 'https://jsonplaceholder.typicode.com';
 
 /* GET api listing. */
 apiRouter.get('/', (req: Request, res: Response) => {
