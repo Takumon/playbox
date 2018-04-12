c43f1d1227a6a4eede80be6d4b659e9787938122
takumon <inouetakumon@gmail.com>
2018-04-12 20:34:43 +0900

    14章分 テスト追加

--- scala-sample/src/test/scala/example/HelloSpec.scala
+++ scala-sample/src/test/scala/example/HelloSpec.scala
@@ -1,6 +1,27 @@
 package example
 
 import org.scalatest._
+import Element.elem
 
-class HelloSpec extends FlatSpec with Matchers {
+
+class HelloSpec extends FunSuite {
+  test("elem result should have passed width") {
+    val ele = elem('x', 2, 3)
+    assert(ele.width == 2)
+  }
+
+  test("elem result should have passed hight") {
+    val ele = elem('x', 2, 3)
+    assertResult(3) {
+      ele.height
+    }
+  }
+
+  test("should throw Exception") {
+    val caught = intercept[ArithmeticException] {
+      1 / 0
+    }
+
+    assert(caught.getMessage == "/ by zero")
+  }
 }
