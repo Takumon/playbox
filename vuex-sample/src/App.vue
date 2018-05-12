b7a703e8ffeec006bf100940e12493defbd6c432
takumon <inouetakumon@gmail.com>
2018-05-12 21:04:56 +0900

    TODOアプリ作成(vuex未導入)

--- vuex-sample/src/App.vue
+++ vuex-sample/src/App.vue
@@ -1,30 +1,39 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+      <Item
+        v-for="item in items"
+        :key="item"
+        :item="item"></Item>
     </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div>
+      <input type="text" v-model="inputTitle">
+      <button @click="addItem">Add</button>
+    </div>
   </div>
 </template>
 
 <script>
+import Item from './Item.vue'
 export default {
   name: 'app',
+  components: { Item },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      inputTitle: "",
+      items: [
+        {is_do: false, title: 'Task1'},
+        {is_do: true, title: 'Task2'},
+        {is_do: false, title: 'Task3'},
+      ]
+    }
+  },
+  methods: {
+    addItem() {
+      this.items.push({
+        title: this.inputTitle,
+        is_do: false
+      })
     }
   }
 }
@@ -40,21 +49,4 @@ export default {
   margin-top: 60px;
 }
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
