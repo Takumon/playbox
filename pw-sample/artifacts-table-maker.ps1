dfdfc6e8e43cb426246b9cbacee6b1a0db871285
Takumon <inouetakumon@gmail.com>
2018-07-08 20:27:30 +0900

    Wiki用テーブル作成処理完成

--- pw-sample/artifacts-table-maker.ps1
+++ pw-sample/artifacts-table-maker.ps1
@@ -21,19 +21,13 @@ function getFileNameAndUrlMap($dirPath, $urlRoot) {
     return $resultMap
 }
 
-
 $artifactsPath = ".\resources\成果物"
 $urlRoot = "https://hogehoge.com/wiki"
 $filenameAndUrlMap = getFileNameAndUrlMap $artifactsPath $urlRoot
 
-
-
-
-
 ############################################################
 # 2 成果物とサンプル、フォーマットのマッピング情報を取得
 ############################################################
-
 
 # 成果物名とサンプル、フォーマットの紐づけをしたマップを取得する
 function getArticatsRelation(
@@ -65,12 +59,8 @@ function getArticatsRelation(
     return $result
 }
 
-
-
 $artifactRelationExcelPath = "file:///C:\Users\Takuto\workspace\git\playbox\pw-sample\resources\成果物紐づけ.xlsx"
 $artifactRelationArray = getArticatsRelation($artifactRelationExcelPath)
-
-
 
 ############################################################
 # 3 成果物一覧情報を取得
@@ -106,111 +96,87 @@ function getArtifactList(
 
 $artifactListExcelPath = "file:///C:\Users\Takuto\workspace\git\playbox\pw-sample\resources\成果物一覧.xlsx"
 $artifactList = getArtifactList($artifactListExcelPath)
-$artifactList | %{
-    echo "▼▼▼▼▼▼▼"
-    echo $_
-    echo "▲▲▲▲▲▲▲"
-}
 
 ############################################################
 # 4 今まで取得した情報をマージ
 ############################################################
-function marge($_artifactList, $_artifactNameList, $_urlList) {
-    $_artifactList | %{ 
-        $name = $_.filename
+$marged = $artifactList | %{ 
+    $name = $_.filename
         
-        # 成果物に対するリンク情報を取得する
-        $url =$_artifactNameList | `
-            ?{ $_.filename -eq $name } | `
-            %{
+    # 成果物に対するリンク情報を取得する
+    $url =$artifactRelationArray | `
+        ?{ $_.filename -eq $name } | `
+        %{
+            @{
                 # 2.サンプル名からURLを取得
-                $sampleUrls = $_.samples | %{
-                    $sampleName = $_
-                    $_urlList | ?{$_.filename -eq $sampleName}
-                }
+                samples=$_.samples | %{ $filenameAndUrlMap.Item($_) };
                 # 3.フォーマット名からURLを取得
-                $formatUrls = $_.formats | %{
-                    $formatName = $_
-                    $_urlList | ?{$_.filename -eq $formatName}
-                }
-
-                @{
-                    samples=$sampleUrls;
-                    formats=$formatUrls;
-                }
+                formats=$_.formats | %{ $filenameAndUrlMap.Item($_) };
             }
+        }
 
-        # 成果物情報にURL情報を付与
-        # $_.add('url', $url)
-        $_
+    # 成果物情報にURL情報を付与
+    $_.add('samplesUrl', $url.samples)
+    $_.add('formatsUrl', $url.formats)
+    $_
+}
+
+
+############################################################
+# 5 マージした情報をWikiのテーブル形式に変換
+############################################################
+function craeteLinks($urls) {
+    # リンクが一つの場合
+    if ($urls -is [System.String]) { 
+        return "|| [$($urls) リンク]"
     }
+    
+    # リンクが複数の場合
+    if ($urls -and $urls.Length) { 
+        $record += "||"
+        for($i = 0; $i -lt $urls.Length; $i++) {
+            $record += "[$($urls[$i]) リンク$($i + 1)]"
+
+            if ($i -ne ($urls.Length -1)) {
+                $record += " "
+            }
+        }
+        return $record
+    }
+    
+     # リンクが存在しない場合
+     return "|| "
 }
 
 
-$marged = marge ($artifactList, $artifactRelationArray, $filenameAndUrlMap)
-
-
-$marged | %{
-    echo "●●●●●●▼▼▼▼▼▼▼"
-    echo $_
-    echo "▲▲▲▲▲▲▲●●●●●"
-
+$tableContent = $marged | %{
+    $record = "|-`r`n"
+    $record += "|$($_.category1)||$($_.category2)||$($_.filename)||$($_.discription)"
+    $record += craeteLinks($_.samplesUrl)
+    $record += craeteLinks($_.fromatsUrl)
+    $record += "|| 備考とか"
+    $record
 }
 
-
-# 指定したディレクトリ配下のエクセルファイルを開いて、エクセルブックのリストを返す
-function openExcels($dirPath) {
-    Get-ChildItem $dirPath -Recurse -Include *.xlsx |
-        % { $_.FullName } |
-        % { $excel.Workbooks.Open($_) }
-}
-
-# $booklist = openExcels "C:\Users\Takuto\Desktop\sample"
-
-# foreach ($book in $booklist) {
-#     $old = $book.Sheets(1).Cells.Item(1,1).value2
-#     If([string]::IsNullOrEmpty($old)) { 
-#         Write-Output "からです"
-#     }
-
-#     $book.Save()
-# }
+$table = @"
+{|
+|大カテゴリ
+|小カテゴリ
+|成果物名
+|サンプル
+|フォーマット
+$($tableContent -join "`r`n")
+|}
+"@
 
 
+###############################################################
+# 6 結果をファイル出力
+###############################################################
 
+$sysdate = Get-Date
 
-# $book = $excel.Workbooks.Open("file:///C:\Users\Takuto\Desktop\sample");
-
-# $book.Sheets.Count # シート数カウント
-# $book.ActiveSheet.Name # アクティブになっているシート名を取得
-# $book.Sheets(1).Name # 最初のシート名を取得
-# $book.Sheets(1).Name = "hoge"
-
-# $book.Sheets(1).Name # 最初のシート名を取得
-
-# $sheet = $book.Sheets("hoge") # 取り扱いやすいようにシート取得します
-# $sheet.Name
-# $sheet.Cells.Item(1,1) = 100 # セルA1に100を代入
-# $sheet.Cells.Item(1,2) = 50 # セルのB1に50を代入
-# $sheet.Range("A2", "B2") = 50 # A2からB2まで50を代入
-# $sheet.Range("A3", "B3") = 5, 10 # A3に5 B3に10を代入
-# $sheet.Range("A2").Text
-# # シートの取り出し
-# # $sheet = $excel.Worksheets.Item("Sheet1")
-# $sheet.Range("C1") = "=SUM(A1:B3)"
-# $sheet.Range("C1").Text # C3 のテキストを表示
-# $sheet.Range("C1").Formula # C3の計算式を表示
-
-# $sheet.Range("C1").copy($sheet.Range("C2:C3")) # 計算式のコピー
-
-# $sheet.Range("C1:C3").Font.Bold = $true # 太字にする
-# $sheet.Range("C1:C3").interior.ColorIndex = 3 # セルを赤色にする
-# $sheet.Range("C1:C3").Font.ColorIndex = 2 # 文字を白色にする
-
-# $sheet.Range("C1:C3") | Get-Member
-# $sheet.Range("A1").AddComment("ほげほげ　ふふふ")
-# $book.SaveAs("${HOME}\Desktop\hoge.xlsx")
-# $excel.Quit() # Excelの修了
-
-# [System.Runtime.Interopservices.Marshal]::ReleaseComObject($excel) # 変数の破棄
-# [System.Runtime.Interopservices.Marshal]::ReleaseComObject($sheet) # 変数の破棄
+$fileName = ".\wiki-table_$($sysdate.ToString("yyyyMMddHHmmss")).txt"
+$file = New-Object System.IO.StreamWriter($fileName, $false, [System.Text.Encoding]::GetEncoding("sjis"))
+$file.Write($table)
+$file.Close()
